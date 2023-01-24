529214a69ec5da1f7f5660bc94b1c72a35707d37
sibtain.raza <sibtain.raza@10pearls.com>
2023-01-24 10:35:18 +0500

    Add exported property for MainActivity

--- Finance.Android/MainActivity.cs
+++ Finance.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Finance.Droid
 {
-    [Activity(Label = "Finance", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Finance", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, Exported = false)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
